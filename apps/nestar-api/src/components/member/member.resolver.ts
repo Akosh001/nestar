79cbeed5d533b8e3f86c22e19d0aaf613dfb4cf1
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-05 18:19:29 +0900

    feat: hashing password via bcryptJs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,26 +10,16 @@ export class MemberResolver {
 
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
-		try {
-			console.log('input', input);
-			console.log('Mutation: signup');
-			return this.memberService.signup(input);
-		} catch (err) {
-			console.log('Error, signup', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('input', input);
+		console.log('Mutation: signup');
+		return this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
-		try {
-			console.log('input', input);
-			console.log('Mutation: login');
-			return this.memberService.login(input);
-		} catch (err) {
-			console.log('Error, login', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('input', input);
+		console.log('Mutation: login');
+		return this.memberService.login(input);
 	}
 
 	@Mutation(() => String)
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,13 +5,18 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enums';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	constructor(
+		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		private authService: AuthService,
+	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
 		try {
+			input.memberPassword = await this.authService.hashPassword(input.memberPassword);
 			const result = await this.memberModel.create(input);
 			return result;
 		} catch (err) {
@@ -32,7 +37,7 @@ export class MemberService {
 			throw new InternalServerErrorException(Message.BLOCKED_USERS);
 		}
 
-		const isMatch = memberPassword === response.memberPassword;
+		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 		return response;
 	}
