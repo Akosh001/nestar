bb690fa71a1e477f47d107af5286c013cde751d0
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-10 23:48:08 +0900

    feat: develop createPOroperty GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Property } from '../../libs/dto/property/property';
+import { Message } from '../../libs/enums/common.enums';
+import { MemberService } from '../member/member.service';
 
 @Injectable()
 export class PropertyService {
-	constructor(@InjectModel('Property') private readonly propertyModel: Model<null>) {}
+	constructor(
+		@InjectModel('Property') private readonly propertyModel: Model<Property>,
+		private memberService: MemberService,
+	) {}
+
+	public async createProperty(input: PropertyInput): Promise<Property> {
+		try {
+			const result = await this.propertyModel.create(input);
+			await this.memberService.memberStatusEditor({ _id: result.memberId, targetKey: 'memberProperties', modifier: 1 });
+			return result;
+		} catch (err) {
+			console.log('Error, Server.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FOUND);
+		}
+	}
 }
