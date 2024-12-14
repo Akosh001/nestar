d55a4f673bc3e846d22b83f50abe506fe9b87399
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-14 19:25:34 +0900

    fix: modify property.service

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -226,19 +226,11 @@ export class PropertyService {
 		return result;
 	}
 	public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property> {
-<<<<<<< HEAD
-		const search: T = { _id: propertyId, propertyStatus: PropertyStatus.DELETE };
-		console.log('searchs', search);
-
-		const result = await this.propertyModel.findOneAndDelete(search).exec();
-		console.log('result:', search);
-=======
 		const search: T = { _id: propertyId, PropertyStatus: PropertyStatus.DELETE };
 		console.log('searchs', search);
 
 		const result = await this.propertyModel.findOneAndDelete(search).exec();
 		console.log('result:', result);
->>>>>>> origin/develop
 
 		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
 		return result;
