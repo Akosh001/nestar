554e50f3822163e20baadadff0f69f8995bb9dd8
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-17 20:15:22 +0900

    feat: integrate comment module related DTOs

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
+import CommentSchema from '../../schemas/Comment.model';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
+		AuthModule,
+		ViewModule,
+		MemberModule,
+	],
+})
 export class CommentModule {}
