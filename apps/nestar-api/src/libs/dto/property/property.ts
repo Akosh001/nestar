45846144f40df538161560d01268f0613b2ccbda
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-18 18:56:18 +0900

    fix: modify getProperty, getBoardArticle business logic

--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -3,6 +3,7 @@ import { ObjectId } from 'mongoose';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
 import { PropertyLocation, PropertyStatus, PropertyType } from '../../enums/property.enum';
 import { Member, TotalCounter } from '../member/member';
+import { MeLiked } from '../like/like';
 
 @ObjectType()
 export class Property {
@@ -79,6 +80,9 @@ export class Property {
 	updatedAt: Date;
 
 	/** from aggrigation**/
+
+	@Field(() => [MeLiked], { nullable: true })
+	meLiked?: MeLiked[];
 
 	@Field(() => Member, { nullable: true })
 	memberData?: Member;
